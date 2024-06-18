4d8fa7379206d73c1607dee292d582ce527add6f
Kathy Zhang <112294375+b27b@users.noreply.github.com>
2024-06-18 06:30:22 -0400

    .

---
+++ seedHelpers.js
@@ -0,0 +1,29 @@
+const descriptors = [
+    'Cozy',
+    'Quiet',
+    'Comfy',
+    'Homey',
+    'Matcha',
+    'Yum',
+    'Honey',
+    'Mood',
+    'Tea',
+    'Rose',
+
+]
+
+const noun = [
+    'City',
+    'Small Town',
+    'Drinks',
+    'Cha',
+    'Talk',
+    'Chat',
+    'Spot',
+    "Co",
+    'and Flower',
+    'Love',
+    'Light',
+    "Food",
+    'Eats'
+]
